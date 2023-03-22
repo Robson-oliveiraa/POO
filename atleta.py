d54af9ff5178451d34e97b32a9d9f9cc8267dc79
Robson-oliveiraa <robsonpvh07@gmail.com>
2023-03-21 23:20:09 -0400

    First Commit

--- atleta.py
+++ atleta.py
@@ -11,9 +11,17 @@ class Atletas:
 
     def carta(self):
 
-        text(f"\033[0;33m\nNome do Atleta: {self.nomeAtleta}\n")
-        text(f"\033[0;33mSexo do(a) Atleta: {self.sexoAtleta} \n")
-        text(f"\033[0;33mForça do(a) Atleta: {self.forcaAtleta} \n")
-        text(f"\033[0;33mMelhor Lançamneto do(a) Atleta: {self.mLancaAtleta} \n")
-        text(f"\033[0;33mCondicionamento Fisico do(a) Atleta: {self.fisicoAtleta} \n")
-        text(f"\033[0;33mTecnica do(a) Atleta: {self.tecnicaAtleta} \n")
+        if self.sexoAtleta == "M":
+            text(f"\033[0;33m\nNome do Atleta: {self.nomeAtleta}\n")
+            text(f"\033[0;33mSexo do Atleta: {self.sexoAtleta} \n")
+            text(f"\033[0;33mForça do Atleta: {self.forcaAtleta} \n")
+            text(f"\033[0;33mMelhor Lançamneto do Atleta: {self.mLancaAtleta} \n")
+            text(f"\033[0;33mCondicionamento Fisico do Atleta: {self.fisicoAtleta} \n")
+            text(f"\033[0;33mTecnica do Atleta: {self.tecnicaAtleta} \n")
+        else:
+            text(f"\033[0;33m\nNome da Atleta: {self.nomeAtleta}\n")
+            text(f"\033[0;33mSexo da Atleta: {self.sexoAtleta} \n")
+            text(f"\033[0;33mForça da Atleta: {self.forcaAtleta} \n")
+            text(f"\033[0;33mMelhor Lançamneto da Atleta: {self.mLancaAtleta} \n")
+            text(f"\033[0;33mCondicionamento Fisico da Atleta: {self.fisicoAtleta} \n")
+            text(f"\033[0;33mTecnica da Atleta: {self.tecnicaAtleta} \n")
