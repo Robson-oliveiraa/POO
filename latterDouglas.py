a818b714eca97e59cb6311c3217383ba69dcb5e7
Robson-oliveiraa <robsonpvh07@gmail.com>
2023-03-23 23:15:20 -0400

    Config

--- latterDouglas.py
+++ latterDouglas.py
@@ -1,6 +1,7 @@
 #initial
 def cartaDouglas():
+
     from classAthlete import athlete
-    #Carta Douglas:
+    
     athleteAndre = athlete("Andre","M","910","15,7m","510","810")
     athleteAndre.exibirInformações() 
--- latterWandell.py
+++ latterWandell.py
@@ -1,4 +1,5 @@
 def cartaWandell():
+    
     from classAthlete import athlete
 
     Darlan = athlete ("Darlan", "m", "940", "16,16 m" ,"750" ,"485")
